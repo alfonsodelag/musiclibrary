e1e16b06bffe3d5845304d995a52dc4ef0786530
Alfonso De La Guardia <alfonsodelag1@gmail.com>
2019-12-16 16:27:35 +0100

    term inserted dynamically to itunes url

---
+++ assets/js/class.js
@@ -0,0 +1,69 @@
+class Search{
+    constructor(entity, term, country, explicit, limit){
+        this.entity = entity;
+        this.term= term;
+        this.country= country;
+        this.explicit= explicit;
+        this.limit= limit;
+    }
+}
+
+// class song extends (artist, album) {
+//     constructor(cover, songName, artistName, albumName, songPrice, releaseDate, songLength, musicalGenre, audioSamples, songLink){
+//         super(artistName, albumName);
+//             this.cover= cover;
+//             this.songName= songName;
+//             this.artistName= artistName;
+//             this.albumName= albumName;
+//             this.songPrice= songPrice;
+//             this.releaseDate= releaseDate;
+//             this.songLength= songLength;
+//             this.musicalGenre= musicalGenre;
+//             this.audioSamples= audioSamples;
+//             this.songLink= songLink;
+//     }
+// }
+
+// class artist extends (song, album) {
+//     constructor(firstName, musicalGenre, artistLink){
+//         this.firstName= firstName;
+//         this.musicalGenre= musicalGenre;
+//         this.artistLink= artistLink;
+//     }
+// }
+
+
+// class album extends (artist, song) {
+//     constructor(cover, albumName, artistName, albumPrice, numberOfSongs, releaseDate, musicalGenre ){
+//         this.cover= cover;
+//         this.songName= songName;
+//         this.artistName= artistName;
+//         this.albumName= albumName;
+//         this.songPrice= songPrice;
+//         this.releaseDate= releaseDate;
+//         this.songLength= songLength;
+//         this.musicalGenre= musicalGenre;
+//         this.audioSamples= audioSamples;
+//         this.songLink= songLink;
+//     }
+// }
+
+// class video extends (artist, song) {
+//     constructor(cover, songName, artistName, songPrice, releaseDate, songLength, musicalGenre, videoSample, videoLink){
+//         this.cover= cover;
+//         this.songName= songName;
+//         this.artistName= artistName;
+//         this.albumName= albumName;
+//         this.songPrice= songPrice;
+//         this.releaseDate= releaseDate;
+//         this.songLength= songLength;
+//         this.musicalGenre= musicalGenre;
+//         this.audioSamples= audioSamples;
+//         this.songLink= songLink;
+//     }
+// }
+
+
+
+
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,66 +1,25 @@
+$("#submit").click(function(e){
 
+    var finder = new Search($("#entity").val(), $("#inputSearch").val(), $("#country").val(), $("#explicit").val(), $("#limit").val());
 
-
-class artist {
-    "first name"
-    "musical genre"
-    "artist link on iTunes"
-}
-
-class song {
-    "song cover"
-    "name of the song"
-    "artist name"
-    "album name"
-    "song price"
-    "release date"
-    "song length"
-    "musical genre"
-    "audio sample of the song"
-    "song link in iTunes"
-
-    getsongname
-
-}
-
-class album {
-    "album cover"
-    "album name"
-    "artist name"
-    "album price"
-    "number of songs"
-    "release date"
-    "musical genre"
-}
-
-class video {
-    "video cover"
-    "name of the song"
-    "artist name"
-    "song Price"
-    "release date"
-    "song length"
-    "musical genre"
-    "clip video sample"
-    "link of the music video on iTunes"
-}
-
-class library{
-
-}
-
-// Using JSONP
-$.ajax({
-    url: "https://itunes.apple.com/search?term=michael+jackson",
- 
-   
- 
-    // Tell jQuery we're expecting JSONP
-    dataType: "jsonp",
- 
- 
-    // Work with the response
-    success: function( response ) {
-        console.log( response ); // server response
-    }
+    e.preventDefault();
+    $.ajax({
+        url: "https://itunes.apple.com/search?term=" + finder.term,
+        dataType: "jsonp",
+        success: function(response){
+            console.log(response);
+        },
+        fail: function(xhr, textStatus, errorThrown){
+        console.log(xhr);
+        }
+    });
 });
+
+
+
+// });
+
+
+
+
+
