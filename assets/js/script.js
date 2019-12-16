0f88648d2ca28189cbde61de7f8b0c4e6a74aa0c
Alfonso De La Guardia <alfonsodelag1@gmail.com>
2019-12-16 18:59:21 +0100

    added ToDo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,19 @@
 $("#submit").click(function(e){
-
     var finder = new Search($("#entity").val(), $("#inputSearch").val(), $("#country").val(), $("#explicit").val(), $("#limit").val());
 
+    console.log($("#limit").val());
     e.preventDefault();
     $.ajax({
-        url: "https://itunes.apple.com/search?term=" + finder.term,
+        url: "https://itunes.apple.com/search?" + "term=" + finder.term + "&" + "country=" + finder.country + "&" + "explicit=" + finder.explicit + "&" + finder.limit,
         dataType: "jsonp",
         success: function(response){
             console.log(response);
         },
-        fail: function(xhr, textStatus, errorThrown){
-        console.log(xhr);
+        fail: function(errorThrown){
+        console.log(errorThrown);
         }
     });
 });
-
-
-
 
 
 
