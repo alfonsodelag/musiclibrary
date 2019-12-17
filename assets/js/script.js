659a4314dbee22629ab75c1075a22cf500a334d5
Alfonso De La Guardia <alfonsodelag1@gmail.com>
2019-12-17 18:35:11 +0100

    fixed countries

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,5 @@
-
-
-
-
+// var content = $('#container');
+       
 // $("#submit").click(function(e){
 //     var finder = new Search($("#entity").val(), $("#inputSearch").val(), $("#country").val(), $("#explicit").val(), $("#limit").val());
 //     e.preventDefault();
@@ -9,82 +7,42 @@
 //     url: "https://itunes.apple.com/search?" + "entity=" + finder.entity + "&" + "term=" + finder.term + "&" + "country=" + finder.country + "&" + "explicit=" + finder.explicit + "&" + "limit=" + finder.limit,        
 //     dataType: "jsonp",
 //         success: function(response){
+//             $.each(response.results, function(i, todo) {
+//             var artistName= todo.artistName;
+//             var albumName= todo.collectionName;
+//             var country= todo.country;
+//             var price= todo.collectionPrice;
+//             var coverImage= todo.artworkUrl100;
+//                 content.append(
+//                     `<div class="row">
+//                     <div class="col-3">
+//                     <img src="${coverImage}"></img>
+//                     <p>Artist Name: ${artistName}</p>
+//                     <p>Album Name: ${albumName}</p>
+//                     <p>Country: ${country}</p>
+//                     <p>Price: ${price}</p>
+//                     </div>
+//                 </div>`)
+//             });
 //             console.log(response);
 //         },
 //         fail: function(errorThrown){
 //         console.log(errorThrown);
 //         }
-//     });
+// });
 // });
 
-{/* <p>${todo.albumName}</p>
-<p>${todo.country}</p>
-<p>${todo.collectionPrice}</p>     */}
 
 
-
-var content = $('#container');
-       
-$("#submit").click(function(e){
-    var finder = new Search($("#entity").val(), $("#inputSearch").val(), $("#country").val(), $("#explicit").val(), $("#limit").val());
-    e.preventDefault();
-    $.ajax({
-    url: "https://itunes.apple.com/search?" + "entity=" + finder.entity + "&" + "term=" + finder.term + "&" + "country=" + finder.country + "&" + "explicit=" + finder.explicit + "&" + "limit=" + finder.limit,        
-    dataType: "jsonp",
-        success: function(response){
-            $.each(response.results, function(i, todo) {
-            var artistName= todo.artistName;
-            var albumName= todo.collectionName;
-            var country= todo.country;
-            var price= todo.collectionPrice;
-            var coverImage= todo.artworkUrl100;
-                content.append(
-                    `<div class="row">
-                    <div class="col-3">
-                    <img src="${coverImage}"></img>
-                    <p>Artist Name: ${artistName}</p>
-                    <p>Album Name: ${albumName}</p>
-                    <p>Country: ${country}</p>
-                    <p>Price: ${price}</p>
-                    </div>
-                </div>`)
-            });
-            console.log(response);
-        },
-        fail: function(errorThrown){
-        console.log(errorThrown);
-        }
-});
-});
-
-
-
-// class song extends (artist, album) {
-//     constructor(cover, songName, artistName, albumName, songPrice, releaseDate, songLength, musicalGenre, audioSamples, songLink){
-//         super(artistName, albumName);
-//             this.cover= cover;
-//             this.songName= songName;
-//             this.artistName= artistName;
-//             this.albumName= albumName;
-//             this.songPrice= songPrice;
-//             this.releaseDate= releaseDate;
-//             this.songLength= songLength;
-//             this.musicalGenre= musicalGenre;
-//             this.audioSamples= audioSamples;
-//             this.songLink= songLink;
-//     }
-// }
-
-
-// function pasteData(){
-//     var paragraph= 
-//     $("#artistName").append(paragraph);
-//     array.forEach(element => {
-        
-//     });
-
-// }
-
+        $.ajax({
+            url: "https://www.liferay.com/api/jsonws/country/get-countries/",
+            // Tell jQuery we're expecting JSONP
+            dataType: "json",
+            // Work with the response
+            success: function( response ) {
+                console.log( response ); // server response
+            }
+        });
 
 // function saludar(nombre){
 //     console.log('Hola' + Alfonso)
@@ -95,6 +53,4 @@ $("#submit").click(function(e){
 
 
 
-
-        
 
