9e2e688438cf4bd42723e108d78720cd53e399a8
Alfonso De La Guardia <alfonsodelag1@gmail.com>
2020-05-07 01:21:23 +0200

    minor fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,56 @@
-
 const path = require("path");
 const webpack = require("webpack");
-const webpack_rules = [];
-const webpackOption = {
-    entry: "./app.js",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js",
-    },
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = {
+    mode: "development",
+    entry: "./src/main.js", // string | object | array
     module: {
-        rules: webpack_rules
-    }
-};
-let babelLoader = {
-    test: /\.js$/,
-    exclude: /(node_modules)/,
-    use: {
-        loader: "babel-loader",
-        options: {
-            presets: ["@babel/preset-env"]
-        }
-    }
-};
-webpack_rules.push(babelLoader);
-module.exports = webpackOption;
+        rules:
+        [
+            {
+                test: /\.js$/, //Regular expression
+                exclude: /(node_modules)/,//excluded node_modules
+                use: {
+                loader: "babel-loader",
+                    options: {
+                    presets: ["@babel/preset-env"]  //Preset used for env setup
+                    }
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader",
+                        options: {
+                            minimize: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    { loader: "css-loader", options: { importLoaders: 1 } },
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        }),
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ]
+}
